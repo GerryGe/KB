5ae91c2f7bda3be3f9a0f6be8e72fb49dcde1679
GerryGe <99432281@qq.com>
2021-01-19 16:41:09 +0800

    Change build path to default and add to gitignore

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Knowledge Base",
   description: 'blog kb knowledge-base wiki',
-  dest: 'public',
+  // dest: 'public', //default build path is ".vuepress/dist/"
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
